51fc4b5e70f326ed31a3ec757c154633d648a25a
Andx <andx667@protonmail.com>
2025-09-23 21:08:28 +0200

    update component name

--- addons/core/script_component.hpp
+++ addons/core/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT addonName
-#define COMPONENT_BEAUTIFIED Addon Name
+#define COMPONENT core
+#define COMPONENT_BEAUTIFIED Core
 #include "\z\aslr\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
