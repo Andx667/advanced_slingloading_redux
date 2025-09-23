08c0ef2c8530ba27af301256082a1867016fd57b
OverlordZorn <dennisAzorn@gmail.com>
2025-09-24 01:47:36 +0200

    improve mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,8 +1,33 @@
-author = "Andx";
 name = "Advanced Slingloading Redux";
-description = "Advanced Slingloading Redux - Version 0.0.0";
-overview = "";
-overviewPicture = "logo_aslr_ca.paa";
-picture = "logo_aslr.png";
-logo = "logo_aslr_ca.paa";
-logoOver = "logo_aslr_ca.paa";
+
+author = "Andx";
+
+picture = "img\logo_aslr.png";                                                                                                              // Picture displayed from the expansions menu. Optimal size is 2048x1024
+hideName        = "false";                                                                                                                  // Hide the extension name
+hidePicture     = "false";                                                                                                                  // Hide the extension menu
+
+logoSmall       = "img\logo_aslr_ca.paa";                                                                                                   // Display next to the item added by the mod 64x64
+logo            = "img\logo_aslr_ca.paa";                                                                                                   // Logo displayed in the main menu 128x128
+logoOver        = "img\logo_aslr_ca.paa";                                                                                                   // When the mouse is over, in the main menu 128x128
+
+tooltip         = "tooltip - If you read this, someone forgot to replace a string in the mod.cpp";
+tooltipOwned    = "tooltipOwned";                                                                                                           // Tool tip displayed when the mouse is left over, in the main menu
+overview        = "overview";                                                                                                               // Overview text, displayed from the extension menu
+description     = "Advanced Slingloading Redux - Version 0.0.0";                                                                            // Probably in context with action
+action          = "https://github.com/Andx667/advanced_slingloading_redux";                                                                 // Website URL, that can accessed from the expansions menu
+actionName      = "Github";                                                                                                                 // label of button/tooltip in extension menu
+
+overviewPicture = "img\logo_aslr_ca.paa";
+overviewText    = "overviewText";
+
+overviewFootnote = "overviewFootnote";
+
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+    0.4117647058823529,
+    0.0,
+    0.0,
+    1
+};
