66331f239870789da6bd9d1adb694b2851082a62
OverlordZorn <dennisAzorn@gmail.com>
2025-09-30 14:57:04 +0200

    Create CfgVehiclesAceActions.hpp

---
+++ addons/core/CfgVehiclesAceActions.hpp
@@ -0,0 +1,40 @@
+// for now during dev only use helicopters
+// TODO  also add to Planes/VTOL later via macro - see example: ACE addons attach cfgVehicles
+#define ROOT_PROPERTIES\
+displayName = "Slingloading Controls";\
+icon = QPATHTOF(data\icon_aslr_ca.paa);\
+statement = "true";\
+condition = "true";\
+showDisabled = 1
+
+
+#define ACTION_DEPLOY\
+    class PVAR(takeFromPack) {\
+        displayName = "something";\
+        condition = "true";\
+        statement = "systemChat 'testsub'";\
+        showDisabled = 1;\
+        icon = QPATHTOF(data\icon_aslr_ca.paa);\
+    }
+
+
+
+
+class Air;
+class Helicopter: Air {
+    class ACE_Actions {
+        class ACE_MainActions {
+            class PVAR(root) {
+                ROOT_PROPERTIES;
+                ACTION_DEPLOY;
+            };
+        };
+    };
+
+    class ACE_SelfActions {
+        class PVAR(root) {
+            ROOT_PROPERTIES;
+            ACTION_DEPLOY;
+        };
+    };
+};
