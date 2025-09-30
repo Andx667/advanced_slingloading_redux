7bac097359ecc18847c0ad20c7e7b6a392eb7f43
OverlordZorn <dennisAzorn@gmail.com>
2025-09-30 14:57:00 +0200

    Update CfgVehicles.hpp

--- addons/core/CfgVehicles.hpp
+++ addons/core/CfgVehicles.hpp
@@ -1,5 +1,7 @@
 class CfgVehicles {
 
+    #include "CfgVehiclesAceActions.hpp"
+
     class Land_Can_V2_F;    // TODO Later: check if Helper_base_F can be viable base class?
     class GVAR(rope_helper): Land_Can_V2_F {
         scope = 1;
