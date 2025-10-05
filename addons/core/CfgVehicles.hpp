63a50c1f7a218fdceae23127b6d12851df45ec34
OverlordZorn <dennisAzorn@gmail.com>
2025-10-05 16:31:41 +0200

    Update CfgVehicles.hpp

--- addons/core/CfgVehicles.hpp
+++ addons/core/CfgVehicles.hpp
@@ -3,7 +3,7 @@ class CfgVehicles {
     #include "CfgVehiclesAceActions.hpp"
 
     class Land_Can_V2_F;    // TODO Later: check if Helper_base_F can be viable base class?
-    class GVAR(rope_helper): Land_Can_V2_F {
+    class GVAR(ropeHelper): Land_Can_V2_F {
         scope = 1;
         author = ECSTRING(main,author);
         displayName = "ASLR Rope Helper";
